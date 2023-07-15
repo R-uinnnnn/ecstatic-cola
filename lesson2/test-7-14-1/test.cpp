7b8a03da9f68bdcd23734d5ff744412cbcf46e78
R-uinnnnn <2669554358@qq.com>
2023-07-15 23:09:00 +0800

    lesson2

--- lesson2/test-7-14-1/test.cpp
+++ lesson2/test-7-14-1/test.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
 	std::cout << "hello sdfdsfdsfds" << std::endl;
-
+	cout << "hjfsdhcnd" << endl;
+	cout << "dhjsa" << endl;
 	return 0;
 }
