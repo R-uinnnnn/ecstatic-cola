3181ce52203c8a3b67ca4383984031c47d324523
R-uinnnnn <2669554358@qq.com>
2023-08-09 22:15:12 +0800

    lesson2

---
+++ lesson2/Sort.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+//插入排序
+void InsertSort(int* a,int n)
+{
+ 
+
+}
+
+//希尔排序
+void ShellSort(int* a,int n)
+{
+
+    
+}
+
+int main()
+{
+
+    return 0;
+}
--- lesson2/test-7-14-1/Tree.cpp
+++ lesson2/test-7-14-1/Tree.cpp
@@ -7,7 +7,23 @@ typedef struct BinaryTree
 	struct BinaryTree* left;//左子树节点
 	struct BinaryTree* right;//右子树节点
 }BTNode;
+BTNode* BuyNode()
+{
+	BTNode* root=(BTNode*)malloc(sizeof(BTNode));
+	if(root==nullptr)
+	{
+		perror("malloc failed!");
+		exit(-1);
+	}
+	root->data=0;
+	root->left=root->right=nullptr;
+}
+BTNode* CreatNode()
+{
+	
 
+
+}
 int TreeSize(int* root,int k)
 {
 	return root==nullptr ? 0 : 
