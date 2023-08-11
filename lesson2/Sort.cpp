f13ad9a2300f140eedc489dcd1f426857eb36897
R-uinnnnn <2669554358@qq.com>
2023-08-11 23:59:52 +0800

    lesson2

--- lesson2/Sort.cpp
+++ lesson2/Sort.cpp
@@ -1,27 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
-//插入排序
+
+/*
 void Swap(int* a,int* b)
 {
     int tmp=*a;
     *a=*b;
     *b=tmp;
 }
+*/
+//插入排序
+//升序排列
+
 void InsertSort(int* a,int n)
 {
     for(int i=0;i<n;i++)
     {
         int end=i;//最后一个元素下标
         int tmp=a[end+1];//要插入的元素
-        while(end>0)
+        while(end>=0&&end+1<n) 
         {
-            if(tmp>a[end])//如果插入元素tmp比前面数据大，则交换
+            if(tmp<a[end])//如果插入元素tmp比前面数据大则交换,小的数据往前面流
             {
-                Swap(&tmp,&(a[end]));
+                a[end+1]=a[end];
+                
             }
-            end--;//确保每个元素都比较了
+            else
+            {
+                break;
+            }
+            end--;//前面数据是有序的
         }
+        a[end+1]=tmp;
     }
 
 }
@@ -29,7 +40,7 @@ void InsertSort(int* a,int n)
 //希尔排序
 void ShellSort(int* a,int n)
 {
-
+    int gap=n/3+1;//gap是跳跃步数，随着循环变化而慢慢变化 
     
 }
 void PrintSort(int* a,int n)
@@ -42,10 +53,10 @@ void PrintSort(int* a,int n)
 }
 int main()
 {
-    int a[10]={2,7,4,8,9,3,4,0,1,5};
-    PrintSort(a,10);
-    InsertSort(a,10);
-    PrintSort(a,10);
+    int a[15]={2,12,45,67,3,92,31,7,87,4,3,14,35,6};
+    PrintSort(a,15);
+    InsertSort(a,15);
+    PrintSort(a,15);
     getchar();
     return 0;
 }
