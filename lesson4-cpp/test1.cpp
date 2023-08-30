305d5293417c968c12f027f594c21797d707e677
R-uinnnnn <2669554358@qq.com>
2023-08-30 22:20:03 +0800

    lesson4-cpp

---
+++ lesson4-cpp/test1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+namespace A
+{
+    int a=100000000;
+}
+
+//using namespace A;//展开命名空间
+//using A::a; n 
+int main() 
+{
+    int a=20;
+    std::cout<< A::a << ": " << a << std::endl;
+    getchar();
+    return 0;
+}
