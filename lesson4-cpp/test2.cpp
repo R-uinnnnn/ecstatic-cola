b8fad7a5b8dbaeaedd8eb4750464541079892f94
R-uinnnnn <2669554358@qq.com>
2023-09-01 18:59:27 +0800

    lesson4-cpp

---
+++ lesson4-cpp/test2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Data
+{
+public:
+    Data()
+    {
+        _year=2;
+        _month=2;
+        _day=2;
+    }
+    Data(int year,int month=1,int day=1)
+    {
+        _year=year;
+        _month=month;
+        _day=day;
+    }
+private:
+    int _year;
+    int _month;
+    int _day;
+
+};
+int main()
+{
+    Data d1(1,2,3);
+    getchar();
+    return 0;
+}
