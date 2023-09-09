a36d41f02430e96eedf61ff617c8443951b9e269
R-uinnnnn <2669554358@qq.com>
2023-09-09 17:03:07 +0800

    lesson4-cpp

--- lesson4-cpp/test2.cpp
+++ lesson4-cpp/test2.cpp
@@ -1,58 +1,39 @@
 #include <iostream>
 using namespace std;
-
 class Data
 {
 public:
-//类里边
-    Data(int year,int month,int day)
+    void Print()//会隐含一个this指针，指向对应的对象
     {
+        cout<<_year<<"-"<<_month<<"-"<<_day<<endl;
+    }
+//我们不写，编译器自动生成一个
+//无参构造函数
+    /*Data()
+    {
+        cout<<"Data();"<<endl;
+        _year=2023;
+        _month=1;
+        _day=1;
+    }*/
+//全缺省构造函数
+    /*Data(int year=2020,int month=2,int day=2)
+    {
+        cout<<"Data(int year=2020,int month=2,int day=2);"<<endl;
         _year=year;
         _month=month;
         _day=day;
-
-    }
-    Data& Add(const Data& d2)
-    {
-        this->_year+=d2._year;
-        //*this._year+=d2._year;
-        this->_month+=d2._month;
-        this->_day+=d2._day;
-        return *this;
-    }
-//private:
+    }*/
+private:
     int _year;
     int _month;
     int _day;
 
 };
-    Data& Add(Data& d1,const Data& d2)
-    {
-        d1._year+=d2._year;
-        //*this._year+=d2._year;
-        d1._month+=d2._month;
-        d1._day+=d2._day;
-        return d1;
-    }
-Data& operator>(Data& d1,Data& d2)
-{
-    if(&d1!=&d2)
-    {
-        d1._day=d2._day;
-        d1._month=d2._month;
-        d1._year=d2._year;
-    }
-    return d1;
-}
-
-
 int main()
 {
-    Data d1(1,1,1);
-    Data d2(2,2,2);
-    d1=d2；
-    cout<<d1._year<<d1._month<<d1._day<<endl;
-    cout<<d2._year<<d2._month<<endl;
+    Data d1;
+    d1.Print();
     getchar();
     return 0;
 }
