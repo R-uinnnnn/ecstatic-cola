a5fbc6b77fb99de57ed38f311de8997b49179a27
R-uinnnnn <2669554358@qq.com>
2023-10-17 23:02:52 +0800

    lesson4

--- lesson4-cpp/String.h
+++ lesson4-cpp/String.h
@@ -1,8 +1,19 @@
+
 namespace STR
 {
     class string
     {
     public:
+    string (const char* str)
+    :_str(new(strlen(str)+1))
+    ,_size(strlen(str))
+    ,_capacity(strlen(str))
+    {
         
+    }
+    private:
+        char* _str;
+        size_t _size;
+        size_t _capacity;
     };
 }
