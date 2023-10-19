8908867b4cce7635eee75650648edd1cba62c632
R-uinnnnn <2669554358@qq.com>
2023-10-20 00:25:26 +0800

    lesson2

--- lesson2/lesson-----cpp/string.cpp
+++ lesson2/lesson-----cpp/string.cpp
@@ -2,27 +2,13 @@
 #include <cstring>
 using namespace std;
 #include "String.h"
-
+void test_string1()
+{
+    abl::string s1("hello world");
+    cout << s1.c_str() << endl;
+}
 int main()
 {
-
-    std::string str1("jkdsajd");
-    std::string str2;
-    cout << str1.c_str() << endl;
-    cout << str2.c_str() << endl;
-
-    abl::string str3("hello world");
-    abl::string::iterator it = str3.begin();
-
-    while (it != str3.end())
-    {
-        cout << *it;
-        it++;
-    }
-    cout << str3.c_str() << endl;
-    str3.insert(5, 5, '%');
-    cout << str3.c_str() << endl;
-    cout << str3[0] << endl;
-    getchar();
+    test_string1();
     return 0;
 }
