4d47c6af3f2577abf2c03a917e77980e460e2977
R-uinnnnn <2669554358@qq.com>
2023-10-19 21:00:40 +0800

    lesson4

--- lesson4-cpp/String.cpp
+++ lesson4-cpp/String.cpp
@@ -5,21 +5,21 @@ using namespace std;
 
 int main()
 {
-    /*
+    
     std::string str1("jkdsajd");
     std::string str2;
     cout<<str1.c_str()<<endl;
     cout<<str2.c_str()<<endl;
-    */
+    
     STR::string str3("hello world");
     STR::string::iterator it=str3.begin();
-    /*
+    
     while(it!=str3.end())
     {
         cout<<*it;
         it++;
     }
-    */
+    
     cout<<str3[0]<<endl;
     getchar();
     return 0;
