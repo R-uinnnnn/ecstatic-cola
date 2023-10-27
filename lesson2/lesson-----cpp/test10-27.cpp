bbb312ecd23ae64081b5583e2a25edbbfee282c0
R-uinnnnn <2669554358@qq.com>
2023-10-28 00:32:03 +0800

    lesson2

--- lesson2/lesson-----cpp/test10-27.cpp
+++ lesson2/lesson-----cpp/test10-27.cpp
@@ -55,10 +55,61 @@ int StrToInt(string str) {
     return num;
 
 }
-int main()
+string addStrings(string num1, string num2)
 {
-    string s1 = "+2147483647";
-    int n = StrToInt(s1);
-    cout << n << endl;
+    size_t i1 = num1.size() - 1;
+    size_t i2 = num2.size() - 1;
+    size_t carry = 0;//进位
+    string num ;
+    int ch = 0;
+    int n1 = 0, n2 = 0;
+    while (i1 != num1.npos || i2 != num2.npos)
+    {
+        if (i1 != num1.npos)
+        {
+            n1 = num1[i1] - '0';
+        }
+        if(i2!=num2.npos)
+            n2 = num2[i2] - '0';
+        if (i1 == num1.npos)
+        {
+            n1 = 0;
+        }
+        else if (i2 == num2.npos)
+        {
+            n2 = 0;
+        }
+        ch = n1 + n2;
+        num += (ch % 10 + carry + '0');
+        if (carry == 1)
+            carry = 0;
+        if (ch / 10 == 1)
+        {
+            carry = 1;
+        }
+
+        if(i1!=num1.npos)
+        i1--;
+        if(i2!=num2.npos)
+        i2--;
+    }
+    if (carry == 1)
+    {
+        num += '1';
+    }
+    string::iterator it1 = num.begin();
+    string::iterator it2 = num.end();
+    reverse(it1, it2);//传递迭代器区间前闭后开
+    return num;
+}
+int main()
+{ 
+    //string s1 = "+2147483647";
+    ////int n = StrToInt(s1);
+    //cout << n << endl;
+    string s1 = "1234";
+    string s2 = "236";
+    string s3 = addStrings(s1, s2);
+    cout << s3 << endl;
     return 0;
 }
