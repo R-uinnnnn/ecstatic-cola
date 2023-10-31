642a29f0809e2aef24dd2148eb5ae84eb1580f8b
R-uinnnnn <2669554358@qq.com>
2023-10-31 23:18:29 +0800

    lesson2

--- lesson2/lesson-----cpp/test10-27.cpp
+++ lesson2/lesson-----cpp/test10-27.cpp
@@ -180,3 +180,20 @@ int main() {
 //
 //    return 0;
 //}
+
+//int singleNumber(vector<int>& nums) {
+//    int i = 0;
+//    int x = 0;
+//    while (i < nums.size())
+//    {
+//        x ^= nums[i++];
+//    }
+//    return x;
+//}
+//int main()
+//{
+//    int a[10] = { 0,1,2,3,4,5,6,7,8,9 };
+//    vector<int> num(a, a + 9);
+//
+//    return 0;
+//}
