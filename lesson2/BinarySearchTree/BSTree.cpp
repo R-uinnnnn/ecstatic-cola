b5396409ca178963382e66f5a47d170da3776559
annn--n <2669554358@qq.com>
2024-01-28 23:08:40 +0800

    lesson2

---
+++ lesson2/BinarySearchTree/BSTree.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+#include "BSTree.h"
+void test1()
+{
+	BSTree<int> n1;
+	n1.Insert(8);
+	n1.Insert(3);
+	n1.Insert(10);
+	n1.Insert(1);
+	n1.Insert(6);
+	n1.Insert(4);
+	n1.Insert(7);
+	n1.Insert(14);
+	n1.Insert(13);
+	n1.InOrder(n1._root);
+	cout << endl;
+	struct BSTreeNode<int>* leftMax = n1._root;
+	//leftMax->_key = 100;
+	//n1.InOrder(n1._root);
+	//cout << endl;
+	//leftMax = leftMax->_left;
+	//leftMax->_key = 110;
+	//n1.InOrder(n1._root);
+	//cout << endl;
+	//leftMax->_left = nullptr;
+	//n1.InOrder(n1._root);
+	//cout << endl;
+	n1.Erase(n1._root, 8);
+	n1.InOrder(n1._root);
+	cout << endl;
+	//n1.Erase(n1._root, 14);
+	//n1.InOrder(n1._root);
+	//cout << endl;
+	//n1.Erase(n1._root, 6);
+	//n1.InOrder(n1._root);
+	//cout << endl;
+	//n1.Erase(n1._root, 10);
+	//n1.InOrder(n1._root);
+	//cout << endl;
+}
+
+int main()
+{
+	test1();
+	return 0;
+}
---
+++ lesson2/BinarySearchTree/BSTree.h
@@ -0,0 +1,142 @@
+#pragma once
+#include <stdio.h>
+template<class T>
+struct BSTreeNode
+{
+	//typedef struct BSTreeNode<T> Node;
+public:
+	BSTreeNode<T>* _left;
+	BSTreeNode<T>* _right;
+	T _key;
+
+	BSTreeNode()
+		:_left(nullptr)
+		,_right(nullptr)
+	{
+
+	}
+	BSTreeNode(T val)
+		:_left(nullptr)
+		, _right(nullptr)
+	{
+		_key = val;
+	}
+
+};
+template<class T>
+class BSTree
+{
+	typedef BSTreeNode<T> Node;
+public:
+	BSTree()
+		:_root(nullptr)//初始化
+	{
+
+	}
+	//BSTree(T val)
+	//	:_root(new Node(T val))//初始化
+	//{
+	//}
+	BSTreeNode<T>* begin()
+	{
+		return _root;
+	}
+	BSTreeNode<T>* end()
+	{
+		return _root->right;
+	}
+	void Insert(T val)
+	{
+		Node* newnode = new Node(val);
+		Node* cur = _root;
+		if (cur == nullptr)
+		{
+			_root = newnode;
+		}
+		while (cur)
+		{
+			if (cur->_key < val)
+			{
+				if (cur->_right == nullptr)
+				{
+					cur->_right = newnode;
+				}
+				else
+				{
+					cur = cur->_right;
+				}
+				
+			}
+			else if (cur->_key > val)
+			{
+				if (cur->_left == nullptr)
+				{
+					cur->_left = newnode;
+				}
+				else
+				{
+					cur = cur->_left;
+				}
+			}
+			else
+			{
+				return;
+			}
+		}
+	}
+	bool Erase(Node*& root,T key)
+	{
+		if (root == nullptr)
+		{
+			return false;
+		}
+		if (root->_key > key)
+		{
+			Erase(root->_left, key);
+		}
+		else if (root->_key < key)
+		{
+			Erase(root->_right, key);
+		}
+		else//找到了要删除的元素位置
+		{
+			Node* dele = root;
+			if (root->_left == nullptr)//左为空
+			{
+				root = root->_right;
+			}
+			else if (root->_right == nullptr)//右为空
+			{
+				root = root->_left;
+			}
+			else//左右都不为空
+			{
+				Node* leftMax = root->_left;
+				while (leftMax->_right != nullptr)//找左子树最大节点（最右节点）/右子树最小节点（最左节点）
+				{
+					leftMax = leftMax->_right;
+				}
+				swap(leftMax->_key, root->_key);
+
+				return Erase(root->_left, key);
+				//不能是Erase(leftMax,key);
+				//return Erase(leftMax, key);
+			}
+		}
+	}
+	
+	void InOrder(Node*& root)
+	{
+		if (root == nullptr)
+		{
+			return;
+		}
+
+		InOrder(root->_left);
+		printf("%d ", root->_key);
+		InOrder(root->_right);
+
+	}
+	Node* _root;
+
+};
