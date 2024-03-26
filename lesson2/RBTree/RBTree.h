c63353525c7b7b86f15c80e964032ef09b1b7a76
annn--n <2669554358@qq.com>
2024-03-26 23:12:39 +0800

    lesson2

--- lesson2/RBTree/RBTree.h
+++ lesson2/RBTree/RBTree.h
@@ -1,8 +1,32 @@
 #pragma once
+enum color
+{
+    red,
+    black
+};
+template<class T>
+struct RBNode          //建立节点
+{
+    RBTree()
+    :_left(nullptr),
+    _right(nullptr),
+    _parent(nullptr),
+    _data(0),
+    _col(red)
+    {
 
+    }
+private:
+    RBNode<T>* _left;
+    RBNode<T>* _right;
+    RBNode<T>* _parent;
+    T _data;
+    color col;
+};
 template<class K,class T>
 struct RBTree
 {
+    typedef struct RBNode Node;
     RBTree(T data)
     :_data(data)
     {
@@ -11,6 +35,6 @@ struct RBTree
     //Find()
 
 private:
-    T _data;//数据
+    Node* _node;//数据
 
 };
