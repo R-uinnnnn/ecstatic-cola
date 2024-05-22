b39587d2797472cfac74d414e13d0e90e6b5a844
annn--n <2669554358@qq.com>
2024-05-22 11:15:40 +0800

    lesson4

--- lesson4-cpp/test1.cpp
+++ lesson4-cpp/test1.cpp
@@ -15,7 +15,7 @@ int main()
 }
 */
 
-
+/*
 
 #include <iostream>
 #include <stdio.h>
@@ -53,12 +53,13 @@ int search_bin(int arr[],int key,int sz)
 
     return 0;
 }
+
 int main()
 {
-	/*    
+	 
 	int arr[8]={1,3,5,7,8,9,10,15};
     int sz = sizeof(arr) / sizeof(arr[0]);
-    search_bin(arr,7,sz);*/
+    search_bin(arr,7,sz);
 	int t=0;
 	int target=-t;
 	cout<<target;
@@ -66,3 +67,76 @@ int main()
 
     return 0;
 }
+*/
+
+#include<iostream>
+using namespace std;
+char X[100];
+char Y[100];
+int c[100][100] = {0};//用于储存X和Y的最长公共子序列的长度
+int b[100][100] = {0};//记录X和Y的最长公共子序列的位置
+void LCSLength(int m,int n,char X[],char Y[],int c[][100],int b[][100])
+{
+	int i,j;
+	//先把第一行和第一列的位置全部变为0
+	for(i=1;i<=m;i++)
+		c[i][0] = 0;
+	for(i=1;i<=n;i++)
+		c[0][i] = 0;
+	for(i=1;i<=m;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(X[i]==Y[j])
+			{
+				c[i][j]=c[i-1][j-1]+1;
+				b[i][j] = 1;//表示最后一个元素相等
+			}
+			else if(c[i-1][j]>=c[i][j-1])
+			{
+				c[i][j]=c[i-1][j];
+				b[i][j]=2;
+			}
+			else
+			{
+				c[i][j]=c[i][j-1];
+				b[i][j]=3;
+			}
+		}
+	}
+}
+void LCS(int i,int j,char X[],int b[][100])
+{
+	if(i==0 ||j==0)
+		return ;
+	if(b[i][j]== 1)
+	{
+		LCS(i-1,j-1,X,b);
+		cout<<X[i];
+	}
+	else if(b[i][j]==2)
+	{
+		LCS(i-1,j,X,b);
+	}
+	else 
+		LCS(i,j-1,X,b);
+}
+int main(void)
+{
+	int i,j,n,m;
+	//先输入x序列的长度在输入y序列的长度
+	//cout<<"先输入x的长度在输入y的长度"<<endl;
+	cin>>m>>n;
+	for(i=1;i<=m;i++)//输入x序列
+		cin>>X[i];
+	for(i=1;i<=n;i++)//输入y序列
+		cin>>Y[i];
+	cout<<endl;
+	LCSLength(m,n,X,Y,c,b);
+	cout<<"最长子序列的长度为："<<c[m][n];
+	cout<<"最长子序列为：";
+	LCS(m,n,X,b);
+	cout<<endl;
+	return 0;
+}
+
